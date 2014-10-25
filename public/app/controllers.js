7afe6083a690845db31cc791e92b3939b63f3eae
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2014-10-25 15:13:39 -0200

    Fix ingredients block

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -3,27 +3,28 @@ angular.module('panelando')
 .controller('IngredientsCtrl', ['$scope', function($scope) {
 	Ingredients = this;
 
-	this.newIngredient = '';
-	this.editingIngredient = '';
+	this.ingredientsList = [{name: 'Leite', editing: false}, {name: 'Açúcar', editing: false}];
+	this.newIngredient = {name: '', editing: false};
 
-	this.ingredientsList = ['Leite', 'Açúcar'];
-
-	this.addIngredient = function(event) {
+		this.addIngredient = function(event) {
 		if(event.keyCode === 13) {
-			Ingredients.ingredientsList.push(Ingredients.newIngredient);
-			Ingredients.newIngredient = '';
+			this.ingredientsList.push(Ingredients.newIngredient);
+			this.newIngredient = {name: '', editing: false};
 		}
 	};
 
 	this.editIngredient = function(index) {
-		Ingredients.editing = index;
+		this.ingredientsList[index].editing = true;
 	};
 
 	this.saveIngredient = function(event, index) {
 		if(event.keyCode === 13) {
-			Ingredients.ingredientsList[index] = Ingredients.editingIngredient[index];
-			console.log(Ingredients.editingIngredient);
-			console.log(Ingredients.ingredientsList)
+			this.ingredientsList[index].editing = false;
 		}
 	};
+
+	this.verify = function() {
+		console.log(Ingredients.ingredientsList);
+	};
+
 }])
