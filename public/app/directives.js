27fe7bfd2e64b118ae6679e8fff9c0d094ef86dd
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2014-10-24 23:37:22 -0200

    Add ingredients directive

--- public/app/directives.js
+++ public/app/directives.js
@@ -1,8 +1,42 @@
 angular.module('panelando')
 
-.directive('ingredientItem', function() {
+// .directive('ingredients', function() {
+// 	return {
+// 		controller: function($scope) {
+// 			var ingredients = $scope.ingredients = [];
+
+// 			this.add
+// 		}
+// 	}
+// })
+
+// .directive('ingredientItem', function($compile) {
+// 	return {
+// 		restrict: 'E',
+// 		template: '<h1>Working</h1>',
+
+// 		link: function(scope, elem, attrs) {
+// 			elem.on('click', function() {
+// 				var newElement = $compile( "<div my-diretive='n'></div>" )( $scope );
+// 				$element.parent().append( newElement );
+// 			})
+// 		}
+// 	}
+// })
+
+.directive('ingredientItem', function($compile) {
 	return {
 		restrict: 'E',
-		template: '<h1>Working</h1>'
-	}
+		template: '<input type="text" ng-keyup="add($event)" autofocus>',
+
+		controller: function($scope, $element) {
+			$scope.add = function(event) {
+				if(event.keyCode === 13) {
+
+				var el = $compile("<ingredient-item></ingredient-item>")($scope);
+				$element.parent().append(el);
+				}
+			};
+		}
+	};
 })
