1c0051166017129ac07296669c8fc68db398dd4c
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2014-10-25 10:37:32 -0200

    Add addIngredient method to ingredients controller

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -3,5 +3,11 @@ angular.module('panelando')
 .controller('IngredientsCtrl', ['$scope', function($scope) {
 	Ingredients = this;
 
+	$scope.newIngredient = '';
+
 	this.ingredientsList = ['20ml de leite', '400g de farinha de trigo', '1 xícara de açúcar'];
+
+	this.addIngredient = function(event) {
+		console.log(event.keyCode);
+	};
 }])
