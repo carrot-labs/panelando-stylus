2ed7995831984c50cbdd3e55d2a3760232a9f7f5
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2014-10-25 11:26:29 -0200

    Update addIngredient method

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -3,11 +3,14 @@ angular.module('panelando')
 .controller('IngredientsCtrl', ['$scope', function($scope) {
 	Ingredients = this;
 
-	$scope.newIngredient = '';
+	this.newIngredient = '';
 
-	this.ingredientsList = ['20ml de leite', '400g de farinha de trigo', '1 xícara de açúcar'];
+	this.ingredientsList = [];
 
 	this.addIngredient = function(event) {
-		console.log(event.keyCode);
+		if(event.keyCode === 13) {
+			Ingredients.ingredientsList.push(Ingredients.newIngredient);
+			Ingredients.newIngredient = '';
+		}
 	};
 }])
